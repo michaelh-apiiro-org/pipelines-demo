16945c6603c441378237c04274b41131a414008a
Tomer Amir <tomer@apiiro.com>
2025-01-28 10:05:41 +0200

    docker setup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const app = require('express')();
 
+process.on('SIGINT', () => process.exit(0));
+
 app.get('/', (req, res) => res.send('success!'));
 
 app.listen(3000, () => console.log('server is up'));
